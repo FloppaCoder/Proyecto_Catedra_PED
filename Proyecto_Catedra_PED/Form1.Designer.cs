c0989c285469e9cf0ea89e0ff4da1b9688138aa3
FloppaCoder <marcelo.menjivar2006@gmail.com>
2025-10-19 01:46:35 -0600

    feat: Dashboardv2

--- Proyecto_Catedra_PED/Form1.Designer.cs
+++ Proyecto_Catedra_PED/Form1.Designer.cs
@@ -29,13 +29,33 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
             textBox1 = new TextBox();
             groupBox2 = new GroupBox();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
             button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
             SuspendLayout();
             // 
             // groupBox1
             // 
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(textBox3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label1);
             groupBox1.Font = new Font("Segoe UI", 16F);
             groupBox1.ForeColor = Color.FromArgb(0, 172, 177);
             groupBox1.Location = new Point(204, 195);
@@ -44,6 +64,66 @@
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Estadisticas";
+            // 
+            // textBox4
+            // 
+            textBox4.BackColor = SystemColors.Control;
+            textBox4.BorderStyle = BorderStyle.None;
+            textBox4.Enabled = false;
+            textBox4.Location = new Point(173, 152);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 29);
+            textBox4.TabIndex = 5;
+            textBox4.Text = "57";
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.Control;
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Enabled = false;
+            textBox3.Location = new Point(173, 106);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 29);
+            textBox3.TabIndex = 4;
+            textBox3.Text = "3";
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = SystemColors.Control;
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Enabled = false;
+            textBox2.Location = new Point(173, 61);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(100, 29);
+            textBox2.TabIndex = 3;
+            textBox2.Text = "12";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(31, 152);
+            label3.Name = "label3";
+            label3.Size = new Size(111, 30);
+            label3.TabIndex = 2;
+            label3.Text = "Atendidos";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(31, 105);
+            label2.Name = "label2";
+            label2.Size = new Size(106, 30);
+            label2.TabIndex = 1;
+            label2.Text = "Urgentes:";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(31, 64);
+            label1.Name = "label1";
+            label1.Size = new Size(123, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Pendientes:";
             // 
             // textBox1
             // 
@@ -64,6 +144,9 @@
             // 
             // groupBox2
             // 
+            groupBox2.Controls.Add(button7);
+            groupBox2.Controls.Add(button6);
+            groupBox2.Controls.Add(button5);
             groupBox2.Font = new Font("Segoe UI", 16F);
             groupBox2.ForeColor = Color.FromArgb(0, 172, 177);
             groupBox2.Location = new Point(675, 195);
@@ -73,28 +156,133 @@
             groupBox2.TabStop = false;
             groupBox2.Text = "Gestion de Turnos";
             // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(0, 172, 177);
+            button7.FlatAppearance.BorderColor = Color.FromArgb(0, 172, 177);
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 172, 177);
+            button7.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 172, 177);
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Segoe UI", 14F);
+            button7.ForeColor = Color.White;
+            button7.Location = new Point(101, 232);
+            button7.Name = "button7";
+            button7.Size = new Size(203, 44);
+            button7.TabIndex = 8;
+            button7.Text = "Limpiar Colas";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(0, 172, 177);
+            button6.FlatAppearance.BorderColor = Color.FromArgb(0, 172, 177);
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 172, 177);
+            button6.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 172, 177);
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Segoe UI", 14F);
+            button6.ForeColor = Color.White;
+            button6.Location = new Point(101, 146);
+            button6.Name = "button6";
+            button6.Size = new Size(203, 44);
+            button6.TabIndex = 7;
+            button6.Text = "Pausar Turno";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(0, 172, 177);
+            button5.FlatAppearance.BorderColor = Color.FromArgb(0, 172, 177);
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 172, 177);
+            button5.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 172, 177);
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI", 14F);
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(101, 64);
+            button5.Name = "button5";
+            button5.Size = new Size(203, 44);
+            button5.TabIndex = 6;
+            button5.Text = "Atender Siguiente";
+            button5.UseVisualStyleBackColor = false;
+            // 
             // button1
             // 
             button1.BackColor = Color.FromArgb(0, 172, 177);
-            button1.FlatStyle = FlatStyle.Flat;
-            button1.FlatAppearance.BorderSize = 0;
             button1.FlatAppearance.BorderColor = Color.FromArgb(0, 172, 177);
+            button1.FlatAppearance.BorderSize = 0;
             button1.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 172, 177);
             button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 172, 177);
+            button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Segoe UI", 14F);
             button1.ForeColor = Color.White;
-            button1.Location = new Point(134, 76);
+            button1.Location = new Point(155, 76);
             button1.Name = "button1";
             button1.Size = new Size(203, 44);
             button1.TabIndex = 2;
             button1.Text = "Registrar Paciente";
             button1.UseVisualStyleBackColor = false;
             // 
+            // button2
+            // 
+            button2.BackColor = Color.FromArgb(0, 172, 177);
+            button2.FlatAppearance.BorderColor = Color.FromArgb(0, 172, 177);
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 172, 177);
+            button2.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 172, 177);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI", 14F);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(391, 76);
+            button2.Name = "button2";
+            button2.Size = new Size(203, 44);
+            button2.TabIndex = 3;
+            button2.Text = "Ver Colas";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(0, 172, 177);
+            button3.FlatAppearance.BorderColor = Color.FromArgb(0, 172, 177);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 172, 177);
+            button3.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 172, 177);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI", 14F);
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(911, 76);
+            button3.Name = "button3";
+            button3.Size = new Size(203, 44);
+            button3.TabIndex = 5;
+            button3.Text = "Salir";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(0, 172, 177);
+            button4.FlatAppearance.BorderColor = Color.FromArgb(0, 172, 177);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatAppearance.MouseDownBackColor = Color.FromArgb(0, 172, 177);
+            button4.FlatAppearance.MouseOverBackColor = Color.FromArgb(0, 172, 177);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI", 14F);
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(675, 76);
+            button4.Name = "button4";
+            button4.Size = new Size(203, 44);
+            button4.TabIndex = 4;
+            button4.Text = "Historial";
+            button4.UseVisualStyleBackColor = false;
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1264, 681);
+            Controls.Add(button3);
+            Controls.Add(button4);
+            Controls.Add(button2);
             Controls.Add(button1);
             Controls.Add(groupBox2);
             Controls.Add(textBox1);
@@ -102,6 +290,9 @@
             Name = "Form1";
             Text = "Sistema de Gestion de Turnos";
             Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
             ResumeLayout(false);
             PerformLayout();
         }
@@ -112,5 +303,17 @@
         private TextBox textBox1;
         private GroupBox groupBox2;
         private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Button button5;
+        private Button button7;
+        private Button button6;
     }
 }
